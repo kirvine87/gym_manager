3c723e7bea0dd0ed691ac3ab50f7b71855b4107a
Kyle <kirvine87@gmail.com>
2019-08-11 15:39:47 +0100

    adds post create

--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -7,7 +7,15 @@ get '/gymclasses' do
 end
 
 #NEW
-get '/gym_classes/new' do
-  @timec = [13.00, 14.00, 16.00 ]
+get '/gymclasses/new' do
+  @timesc = [13.00, 14.00, 16.00 ]
   erb(:"gym_classes/new")
+end
+
+#CREATE
+post '/gymclasses' do
+  @timesc = [13.00, 14.00, 16.00 ]
+  gclass = GymClass.new(params)
+  gclass.save()
+  redirect "/gymclasses"
 end
